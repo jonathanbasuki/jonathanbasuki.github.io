235fec76fc0c0970b178d09ff1d3634ba5fda2a2
jonathanbasuki <basukijonathan4@gmail.com>
2022-04-22 19:48:06 +0700

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      fontSize: {
+        base: ['18px', '25px'],
+        lg: ['24px', '33px'],
+      },
+      colors: {
+        'primary': '#000000',
+        'secondary': '#2D4263',
+      },
+    },
+  },
+  plugins: [],
+}
